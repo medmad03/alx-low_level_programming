c06bc382726fd1787ade70d9280bdf2c41106f9d
medmad03 <mahmed300@stu.ui.edu.ng>
2022-10-19 16:54:45 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lower case
+*/
+void print_alphabet(void)
+{
+	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+		_putchar(la);
+
+	_putchar('\n');
+}
